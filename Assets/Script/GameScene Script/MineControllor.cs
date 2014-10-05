5273790745ce4ecd67071b1890ca1dc387105a53
mgskai <mgskai@gmail.com>
2014-10-04 23:17:20 -0400

    Start

---
+++ Assets/Script/GameScene Script/MineControllor.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MineControllor : MonoBehaviour {
+
+    public TextMesh mineText;
+
+    void start()
+    {
+        renderer.material.color = Color.green;
+        mineText.text = "2";
+    }
+
+    void OnMouseEnter()
+    {
+        renderer.material.color = Color.cyan;
+    }
+
+    void OnMouseExit()
+    {
+        renderer.material.color = Color.green;
+    }
+
+    void OnMouseUp()
+    {
+        if (mineText.text == "3")
+        {
+            mineText.text = "2";
+        } 
+        else
+        {
+            mineText.text = "3";
+        }
+    }
+}
---
+++ Assets/Script/TitleScene Script/NewGameController.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class NewGameController : MonoBehaviour 
+{
+    void Start()
+    {
+        renderer.material.color = Color.blue;
+    }
+    void OnMouseEnter()
+    {
+        renderer.material.color = Color.red;
+    }
+
+    void OnMouseDown()
+    {
+        Application.LoadLevel("gameScene");
+    }
+
+    void OnMouseExit()
+    {
+        renderer.material.color = Color.blue;
+    }
+}
